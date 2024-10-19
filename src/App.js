44e23d4dfdf0faf6fb6d364f70430f036dbbf0f2
Alexandru Nastase <nastase.alexandru100@gmail.com>
2024-10-19 20:59:59 +0100

    useEffect()

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import UseStateHook from './hooks/01_useState/UseStateHook';
-import UseEffectHook from './hooks/02_UseEffectHook';
+import UseEffectHook from './hooks/02_useEffect/UseEffectHook';
 
 function App() {
   return (
     <>
       <h1>Hooks Tutorial</h1>
       <UseStateHook />
-      {/* <UseEffectHook /> */}
+      <UseEffectHook />
     </>
   )
 }
